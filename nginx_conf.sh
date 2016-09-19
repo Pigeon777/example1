ccd3fd51632a8bad01c4a49c81aaf8e76e484a37
Pigeon777 <pigeon777@gmail.com>
2016-09-19 09:25:51 +0300

    Add files via upload

--- nginx_conf.sh
+++ nginx_conf.sh
@@ -6,9 +6,9 @@ mkdir /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 
 wget https://github.com/Pigeon777/example1/raw/master/nginx.conf
-wget https://github.com/Pigeon777/example1/raw/master/example-site.conf
+wget https://github.com/Pigeon777/example1/raw/master/example.conf
 cp -f nginx.conf /etc/nginx/nginx.conf 
-cp -f example-site.conf /etc/nginx/sites-available/example-site.conf
+cp -f example.conf /etc/nginx/sites-available/example.conf
 ln -s /etc/nginx/sites-available/example.conf /etc/nginx/sites-enabled/example.conf
 
 service nginx reload
