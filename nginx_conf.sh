204388001ffa01ec22e8b97079de0ff5dac7caa0
Pigeon777 <pigeon777@gmail.com>
2016-09-19 08:41:12 +0300

    Add files via upload

--- nginx_conf.sh
+++ nginx_conf.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 apt-get install Nginx
+mkdir /etc/nginx/ssl
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 
-wget https://github.com/Pigeon777/example1/raw/master/nginx.conf /var/tmp/
-wget https://github.com/Pigeon777/example1/raw/master/example-site.conf /var/tmp/
-cp -f /var/tmp/nginx.conf /etc/nginx/nginx.conf 
-cp -f /var/tmp/example-site.conf /etc/nginx/sites-available/example-site.conf
+wget https://github.com/Pigeon777/example1/raw/master/nginx.conf
+wget https://github.com/Pigeon777/example1/raw/master/example-site.conf
+cp -f nginx.conf /etc/nginx/nginx.conf 
+cp -f example-site.conf /etc/nginx/sites-available/example-site.conf
